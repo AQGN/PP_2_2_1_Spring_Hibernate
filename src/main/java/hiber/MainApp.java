4e93611d9401a843c5b6fdc199b30aa7b5426d5f
Albert Isaev <thealbertisaev@gmail.com>
2023-01-18 21:57:55 +0500

    update

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -5,12 +5,10 @@ import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
@@ -23,12 +21,7 @@ public class MainApp {
 
         List<User> users = userService.listUsers();
         for (User user : users) {
-            System.out.println("Id = " + user.getId());
-            System.out.println("First Name = " + user.getFirstName());
-            System.out.println("Last Name = " + user.getLastName());
-            System.out.println("Email = " + user.getEmail());
-            System.out.println("Car = " + user.getCar());
-            System.out.println();
+            System.out.println(user.toString());
         }
 
         System.out.println(userService.getUserByCar("ThirdCar", 300));
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -86,6 +86,7 @@ public class User {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
+                ", car=" + car +
                 '}';
     }
 }
